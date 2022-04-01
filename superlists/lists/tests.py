1a3e74edf850a5dd18fc44b87cfe458669b0e7c4
Jian-G <541832360@qq.com>
2022-04-01 14:41:47 +0800

    Basic view now returns minial HTML

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,25 @@
+import imp
+from urllib import response
+from django.http import HttpRequest
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
+from django.http import HttpRequest
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+
+class HomePageTest(TestCase):
+    
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def text_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf-8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title> To-Do lists</title>", html)
+        self.assertTrue(html.endswith("</html>"))
